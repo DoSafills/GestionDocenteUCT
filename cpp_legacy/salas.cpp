c50401a6983a82d131deb3f6f5e2ef162337e3ab
Kevin <kparra2023@alu.uct.cl>
2025-10-06 23:07:27 -0300

    chore: delete unused files

--- cpp_legacy/salas.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>  // Librería para entrada y salida de datos
-
-int main() {
-    std::cout << "Hola, mundo!" << std::endl; // Imprime en pantalla
-    return 0; // Indica que el programa terminó correctamente
-}
