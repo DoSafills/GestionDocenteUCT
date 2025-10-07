bbfe708c25dc146ff06a83c7d715244e8339ff97
Kevin <kparra2023@alu.uct.cl>
2025-10-06 23:20:05 -0300

    chore: fix typo

--- Sistema_Basico/src/components/ui/button.tsx
+++ Sistema_Basico/src/components/ui/button.tsx
@@ -8,7 +8,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground-alt hover:bg-primary/90",
         outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         ghost: "hover:bg-accent hover:text-accent-foreground",
 		// se pueden agregar mas variantes
