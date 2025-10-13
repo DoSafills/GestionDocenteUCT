e57544a40d790d51fb1054f309e0eade6373a2aa
Cristian Cáceres <134634536+Cristian0Caceres@users.noreply.github.com>
2025-10-12 21:09:49 -0300

    :]

--- frontend/src/api/restricciones.ts
+++ frontend/src/api/restricciones.ts
@@ -4,17 +4,11 @@ import { ENDPOINTS } from '@endpoints/index';
 import { ApiService } from '../../../frontend/src/pages/RestriccionesPage/services/apiservice';
 import type { RestriccionDTO } from '../../../frontend/src/pages/RestriccionesPage/services/RestriccionDTO';
 
-/**
- * Clase especializada para manejar las operaciones de restricciones académicas.
- * Extiende de ApiService con tipado específico de RestriccionDTO.
- */
 export class RestriccionApiService extends ApiService<RestriccionDTO> {
     constructor() {
-        super(ENDPOINTS.RESTRICCIONES); // usar la propiedad correcta del endpoint
+        super(ENDPOINTS.RESTRICCIONES); 
     }
 }
 
-/**
- * Instancia lista para usar en toda la aplicación.
- */
+
 export const restriccionApiService = new RestriccionApiService();
